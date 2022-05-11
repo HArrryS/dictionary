866918fccc5b5cfa2bf47493a5410d1dbc0a5274
HarrryS <18052@wc.school.nz>
2022-05-12 11:10:10 +1200

    updata the foot note

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ bcrypt = Bcrypt(app)
 DATABASE = "C:/Users/18052/OneDrive - Wellington College/13DTS/dictionary/Harry.db"
 app.secret_key = "banana"
 
-
+# enable the foreign keys
 def create_connection(db_file):
     try:
         connection = sqlite3.connect(db_file)
@@ -21,16 +21,17 @@ def create_connection(db_file):
     return None
 
 
+# app route to the main page
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
     if request.method == 'POST':
         print(request.form)
-        category = request.form.get('category').title().strip()
-
+        category = request.form.get('category').title().strip()# gets the category from
+        # connect to database
         con = create_connection(DATABASE)
-        query = "INSERT INTO categories (category) VALUES (?)"
+        query = "INSERT INTO categories (category) VALUES (?)" # insert cateories into my database
         cur = con.cursor()
-        cur.execute(query, (category,))
+        cur.execute(query, (category,)) # excute the query
         con.commit()
         con.close()
         return redirect('/')
@@ -39,45 +40,48 @@ def hello_world():
     if error is None:
         error = ""
 
+    # connect to database
     con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC"
+    query = "SELECT id, category FROM categories ORDER BY category ASC"  # select category fom
     cur = con.cursor()
     cur.execute(query)
-
     category_list = cur.fetchall()
     cur.close()
 
-    return render_template('home.html', error=error, categories=category_list, logged_in=is_logged_in(), logged_in_teacher=is_logged_in_teacher())
+    return render_template('home.html', error=error, categories=category_list, logged_in=is_logged_in(), logged_in_teacher=is_logged_in_teacher(), )
 
-
+# app route to contact
 @app.route('/contact')
 def contact():
+    # connect to database
     con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC"
+    query = "SELECT id, category FROM categories ORDER BY category ASC" # select category from database
     cur = con.cursor()
-    cur.execute(query)
-
+    cur.execute(query) # execute the query
     category_list = cur.fetchall()
     cur.close()
     return render_template("contact.html", categories=category_list, logged_in=is_logged_in(), logged_in_teacher=is_logged_in_teacher())
 
-
+# app route to login
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    # if logged in user are not able to come back to login page
     if is_logged_in():
         return redirect('/')
 
     if request.method == 'POST':
-        email = request.form.get('email').lower().strip()
-        password = request.form.get('password')
+        email = request.form.get('email').lower().strip() # gets the email from the form
+        password = request.form.get('password') # gets the password from the form
 
+        # creat connection to database
         con = create_connection(DATABASE)
-        query = "SELECT id, fname, password, lname, teacher FROM User WHERE email=?"
+        query = "SELECT id, fname, password, lname, teacher FROM User WHERE email=?" # select user data from database
         cur = con.cursor()
-        cur.execute(query, (email,))
+        cur.execute(query, (email,)) # excute the query
         user_data = cur.fetchall()
         con.close()
 
+        # check if userdata are same to the data base
         if user_data:
             user_id = user_data[0][0]
             fname = user_data[0][1]
@@ -89,9 +93,11 @@ def login():
         else:
             return redirect("/login?error=Incorrect+username+or+password")
 
+        # if password are not same
         if not bcrypt.check_password_hash(db_password, password):
             return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
 
+        # create session for user
         session['email'] = email
         session['userid'] = user_id
         session['first_name'] = fname
@@ -101,22 +107,23 @@ def login():
 
         return redirect("/")
 
+    # connect to database
     con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC"
+    query = "SELECT id, category FROM categories ORDER BY category ASC"  # select category from database
     cur = con.cursor()
-    cur.execute(query)
-
+    cur.execute(query)  # execute the query
     category_list = cur.fetchall()
     cur.close()
 
     return render_template("login.html", categories=category_list, logged_in=is_logged_in(), logged_in_teacher=is_logged_in_teacher())
 
-
+# app route to signup
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
+    # if logged in, user shouldn't be able to signup
     if is_logged_in():
         return redirect('/')
-
+    # gets data from form
     if request.method == 'POST':
         print(request.form)
         fname = request.form.get('fname').title().strip()
@@ -126,54 +133,54 @@ def signup():
         password2 = request.form.get('password2')
         role = request.form.get('role')
 
-        if len(fname) < 2:
+        if len(fname) < 2: # first name has to be more than one character
             return redirect("\signup?error=FirstName+must+be+at+least+2+charactes")
 
-        if len(lname) < 2:
+        if len(lname) < 2: # last name has to be more than one character
             return redirect("\signup?error=LastName+must+be+at+least+2+charactes")
 
-        if password2 != password:
-            return redirect("\signup?error=LastName+must+be+at+least+2+charactes")
+        if password2 != password: # check if the pass word are the same
+            return redirect("\signup?error=Password+is+wrong")
 
-        if len(password) < 8:
+        if len(password) < 8: # password have to be more than seven characters
             return redirect("\signup?error=Password+must+be+at+least+8+characters")
 
-        hashed_password = bcrypt.generate_password_hash(password)
+        hashed_password = bcrypt.generate_password_hash(password) # hash the password, protects user password.
 
         con = create_connection(DATABASE)
+        # insert user data to database
         query = "INSERT INTO User (fname, lname, email, password, teacher) VALUES (?,?,?,?,?)"
 
         cur = con.cursor()
         try:
-            cur.execute(query, (fname, lname, email, hashed_password, role))
+            cur.execute(query, (fname, lname, email, hashed_password, role)) # execute the query
         except sqlite3.IntegrityError:
             return redirect('/signup?error=Email+is+already+used')
         con.commit()
         con.close()
         return redirect('/login')
 
-        return render_template("signup.html")
-
+    # connect to database
     con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC"
+    query = "SELECT id, category FROM categories ORDER BY category ASC"  # select category from database
     cur = con.cursor()
-    cur.execute(query)
-
+    cur.execute(query)  # execute the query
     category_list = cur.fetchall()
     cur.close()
     return render_template('signup.html', logged_in=is_logged_in(), categories=category_list, logged_in_teacher=is_logged_in_teacher())
 
-
+# app route logout
 @app.route('/logout')
 def logout():
     print(list(session.keys()))
-    [session.pop(key) for key in list(session.keys())]
+    [session.pop(key) for key in list(session.keys())] # quit session
     print(list(session.keys()))
     return redirect(request.referrer + '?message=See+you+next+time!')
 
-
+# app route category
 @app.route('/category/<category_id>', methods=['POST', 'GET'])
 def category(category_id):
+    # get data from form
     if request.method == 'POST':
         print(request.form)
         maori = request.form.get('maori').title().strip()
@@ -186,14 +193,16 @@ def category(category_id):
         username = session['first_name'] + " " +session['last_name']
         noimage = "noimage"
 
+        # max level is ten, so user can't add a word with level higher than ten
         if int(level) > 10:
             return redirect("/category/{}?error=level+must+be+less+than+10".format(category_id))
 
         con = create_connection(DATABASE)
+        # insert the data to the database
         query = "INSERT INTO Words (maori, english, level, definition, timestamp, category_id, user_id, username, image) VALUES (?,?,?,?,?,?,?,?,?)"
 
         cur = con.cursor()
-
+        # execute the query
         cur.execute(query, (maori, english, level, definition, timestamp, category, user_id, username, noimage))
 
         con.commit()
@@ -201,11 +210,11 @@ def category(category_id):
 
     # connect to database
     con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC"
+    query = "SELECT id, category FROM categories ORDER BY category ASC" # select data from database
     cur = con.cursor()
     cur.execute(query)
     category_list = cur.fetchall()
-    query = "SELECT id, category FROM categories WHERE id = ?"
+    query = "SELECT id, category FROM categories WHERE id = ?" # select data from database
     cur.execute(query, (category_id,))
     category = cur.fetchall()
     print(category_id, category)
@@ -221,9 +230,10 @@ def category(category_id):
     return render_template('category.html', categories=category_list, words_list=words_list, category=category[0],
                            logged_in=is_logged_in(), catID=category_id, logged_in_teacher=is_logged_in_teacher())
 
-
+# app route to words
 @app.route('/word/<wordid>', methods=['POST', 'GET'])
 def word(wordid):
+    # get data from form
     if request.method == 'POST':
         print(request.form)
         maori = request.form.get('maori').title().strip()
@@ -236,10 +246,11 @@ def word(wordid):
         word_id = wordid
 
         con = create_connection(DATABASE)
+        # replace the word in the database
         query = "UPDATE Words SET maori = ?, english =?, level= ?, definition = ?, timestamp = ?, user_id = ?, " \
                 "username = ? WHERE id = ? "
         cur = con.cursor()
-        cur.execute(query, (maori, english, level, definition, timestamp, user_id, username, word_id))
+        cur.execute(query, (maori, english, level, definition, timestamp, user_id, username, word_id)) # execute the query
         con.commit()
         con.close()
 
@@ -251,13 +262,14 @@ def word(wordid):
     words = cur.fetchall()
     con.close
 
+    # connect to database
     con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC"
+    query = "SELECT id, category FROM categories ORDER BY category ASC"  # select category from database
     cur = con.cursor()
-    cur.execute(query)
-
+    cur.execute(query)  # execute the query
     category_list = cur.fetchall()
     cur.close()
+
     return render_template('word.html', logged_in=is_logged_in(), categories=category_list, words=words, logged_in_teacher=is_logged_in_teacher())
 
 @app.route('/deleteword/<word_id>/<maori>')
@@ -271,13 +283,14 @@ def delete_word(word_id, maori):
     words = cur.fetchall()
     con.close
 
+    # connect to database
     con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC"
+    query = "SELECT id, category FROM categories ORDER BY category ASC"  # select category from database
     cur = con.cursor()
-    cur.execute(query)
-
+    cur.execute(query)  # execute the query
     category_list = cur.fetchall()
     cur.close()
+
     return render_template("deleteword.html", categories=category_list, logged_in=is_logged_in(), words=words, logged_in_teacher=is_logged_in_teacher())
 
 @app.route('/deleteword/<word_id>')
@@ -335,6 +348,7 @@ def is_logged_in():
     else:
         print("logged in")
         return True
+
 def is_logged_in_teacher():
     if session.get("teacher") == "Y":
         print("logged in as teacher")
