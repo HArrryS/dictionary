7848df7b9831531cd2721358e3712d76dfcd3532
HArrryS <18052@wc.school.nz>
2022-05-15 22:21:45 +1200

    added function of category_list. Add search.html

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
-DATABASE = "C:/Users/18052/OneDrive - Wellington College/13DTS/dictionary/Harry.db"
+DATABASE = "C:/Users/28014/OneDrive - Wellington College/13DTS/dictionary/Harry.db"
 app.secret_key = "banana"
+
 
 # enable the foreign keys
 def create_connection(db_file):
@@ -21,17 +22,54 @@ def create_connection(db_file):
     return None
 
 
+# logging state of the user
+def is_logged_in():
+    if session.get("email") is None:  # if there is no email the user are not logged in
+        print("not logged in")
+        return False
+    else:
+        print("logged in")
+        return True
+
+
+# logging as a teacher or a student
+def is_logged_in_teacher():
+    if session.get("teacher") == "Y":  # if the user is the teacher
+        print("logged in as teacher")
+        return True
+    else:
+        print("logged in as student")
+        return False
+
+
+def category_list():
+    con = create_connection(DATABASE)
+    query = "SELECT id, category FROM categories ORDER BY category ASC"  # select category fom
+    cur = con.cursor()
+    cur.execute(query)
+    category_list = cur.fetchall()
+    cur.close()
+    return category_list
+
+
 # app route to the main page
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
+
+    return render_template('home.html', categories=category_list(), logged_in=is_logged_in(),
+                           logged_in_teacher=is_logged_in_teacher(), )
+
+
+@app.route('/add_category', methods=['POST', 'GET'])
+def add_category():
     if request.method == 'POST':
         print(request.form)
-        category = request.form.get('category').title().strip()# gets the category from
+        category = request.form.get('category').title().strip()  # gets the category from
         # connect to database
         con = create_connection(DATABASE)
-        query = "INSERT INTO categories (category) VALUES (?)" # insert cateories into my database
+        query = "INSERT INTO categories (category) VALUES (?)"  # insert categories into my database
         cur = con.cursor()
-        cur.execute(query, (category,)) # excute the query
+        cur.execute(query, (category,))  # execute the query
         con.commit()
         con.close()
         return redirect('/')
@@ -40,29 +78,19 @@ def hello_world():
     if error is None:
         error = ""
 
-    # connect to database
-    con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC"  # select category fom
-    cur = con.cursor()
-    cur.execute(query)
-    category_list = cur.fetchall()
-    cur.close()
+    return render_template('add_category.html', error=error, categories=category_list(), logged_in=is_logged_in(),
+                           logged_in_teacher=is_logged_in_teacher(), )
 
-    return render_template('home.html', error=error, categories=category_list, logged_in=is_logged_in(), logged_in_teacher=is_logged_in_teacher(), )
 
 # app route to contact
 @app.route('/contact')
 def contact():
-    # connect to database
-    con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC" # select category from database
-    cur = con.cursor()
-    cur.execute(query) # execute the query
-    category_list = cur.fetchall()
-    cur.close()
-    return render_template("contact.html", categories=category_list, logged_in=is_logged_in(), logged_in_teacher=is_logged_in_teacher())
 
-# app route to login
+    return render_template("contact.html", categories=category_list(), logged_in=is_logged_in(),
+                           logged_in_teacher=is_logged_in_teacher())
+
+
+# app route to log in
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     # if logged in user are not able to come back to login page
@@ -70,14 +98,14 @@ def login():
         return redirect('/')
 
     if request.method == 'POST':
-        email = request.form.get('email').lower().strip() # gets the email from the form
-        password = request.form.get('password') # gets the password from the form
+        email = request.form.get('email').lower().strip()  # gets the email from the form
+        password = request.form.get('password')  # gets the password from the form
 
         # creat connection to database
         con = create_connection(DATABASE)
-        query = "SELECT id, fname, password, lname, teacher FROM User WHERE email=?" # select user data from database
+        query = "SELECT id, fname, password, lname, teacher FROM User WHERE email=?"  # select user data from database
         cur = con.cursor()
-        cur.execute(query, (email,)) # excute the query
+        cur.execute(query, (email,))  # excute the query
         user_data = cur.fetchall()
         con.close()
 
@@ -107,20 +135,14 @@ def login():
 
         return redirect("/")
 
-    # connect to database
-    con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC"  # select category from database
-    cur = con.cursor()
-    cur.execute(query)  # execute the query
-    category_list = cur.fetchall()
-    cur.close()
+    return render_template("login.html", categories=category_list(), logged_in=is_logged_in(),
+                           logged_in_teacher=is_logged_in_teacher())
 
-    return render_template("login.html", categories=category_list, logged_in=is_logged_in(), logged_in_teacher=is_logged_in_teacher())
 
 # app route to signup
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
-    # if logged in, user shouldn't be able to signup
+    # if logged in, user shouldn't be able to sign up
     if is_logged_in():
         return redirect('/')
     # gets data from form
@@ -133,19 +155,19 @@ def signup():
         password2 = request.form.get('password2')
         role = request.form.get('role')
 
-        if len(fname) < 2: # first name has to be more than one character
+        if len(fname) < 2:  # first name has to be more than one character
             return redirect("\signup?error=FirstName+must+be+at+least+2+charactes")
 
-        if len(lname) < 2: # last name has to be more than one character
+        if len(lname) < 2:  # last name has to be more than one character
             return redirect("\signup?error=LastName+must+be+at+least+2+charactes")
 
-        if password2 != password: # check if the pass word are the same
+        if password2 != password:  # check if the pass word are the same
             return redirect("\signup?error=Password+is+wrong")
 
-        if len(password) < 8: # password have to be more than seven characters
+        if len(password) < 8:  # password have to be more than seven characters
             return redirect("\signup?error=Password+must+be+at+least+8+characters")
 
-        hashed_password = bcrypt.generate_password_hash(password) # hash the password, protects user password.
+        hashed_password = bcrypt.generate_password_hash(password)  # hash the password, protects user password.
 
         con = create_connection(DATABASE)
         # insert user data to database
@@ -153,29 +175,25 @@ def signup():
 
         cur = con.cursor()
         try:
-            cur.execute(query, (fname, lname, email, hashed_password, role)) # execute the query
+            cur.execute(query, (fname, lname, email, hashed_password, role))  # execute the query
         except sqlite3.IntegrityError:
             return redirect('/signup?error=Email+is+already+used')
         con.commit()
         con.close()
         return redirect('/login')
 
-    # connect to database
-    con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC"  # select category from database
-    cur = con.cursor()
-    cur.execute(query)  # execute the query
-    category_list = cur.fetchall()
-    cur.close()
-    return render_template('signup.html', logged_in=is_logged_in(), categories=category_list, logged_in_teacher=is_logged_in_teacher())
+    return render_template('signup.html', logged_in=is_logged_in(), categories=category_list(),
+                           logged_in_teacher=is_logged_in_teacher())
+
 
 # app route logout
 @app.route('/logout')
 def logout():
     print(list(session.keys()))
-    [session.pop(key) for key in list(session.keys())] # quit session
+    [session.pop(key) for key in list(session.keys())]  # quit session
     print(list(session.keys()))
     return redirect(request.referrer + '?message=See+you+next+time!')
+
 
 # app route category
 @app.route('/category/<category_id>', methods=['POST', 'GET'])
@@ -188,9 +206,9 @@ def category(category_id):
         level = request.form.get('level').lower().strip()
         definition = request.form.get('definition')
         timestamp = datetime.now()
-        category =category_id
+        category = category_id
         user_id = session['userid']
-        username = session['first_name'] + " " +session['last_name']
+        username = session['first_name'] + " " + session['last_name']
         noimage = "noimage"
 
         # max level is ten, so user can't add a word with level higher than ten
@@ -199,7 +217,8 @@ def category(category_id):
 
         con = create_connection(DATABASE)
         # insert the data to the database
-        query = "INSERT INTO Words (maori, english, level, definition, timestamp, category_id, user_id, username, image) VALUES (?,?,?,?,?,?,?,?,?)"
+        query = "INSERT INTO Words (maori, english, level, definition, timestamp, category_id, user_id, username, " \
+                "image) VALUES (?,?,?,?,?,?,?,?,?) "
 
         cur = con.cursor()
         # execute the query
@@ -210,25 +229,22 @@ def category(category_id):
 
     # connect to database
     con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC" # select data from database
+    query = "SELECT id, category FROM categories WHERE id = ?"  # select data from database
     cur = con.cursor()
-    cur.execute(query)
-    category_list = cur.fetchall()
-    query = "SELECT id, category FROM categories WHERE id = ?" # select data from database
     cur.execute(query, (category_id,))
     category = cur.fetchall()
     print(category_id, category)
-
     # Select the data from my database
     query = "SELECT id, maori, english, image FROM Words WHERE category_id=? ORDER BY maori ASC"
 
     cur = con.cursor()
     cur.execute(query, (category_id,))  # executes the query
     words_list = cur.fetchall()  # put result into a list
-    con.close
+    con.close()
 
-    return render_template('category.html', categories=category_list, words_list=words_list, category=category[0],
+    return render_template('category.html', categories=category_list(), words_list=words_list, category=category[0],
                            logged_in=is_logged_in(), catID=category_id, logged_in_teacher=is_logged_in_teacher())
+
 
 # app route to words
 @app.route('/word/<wordid>', methods=['POST', 'GET'])
@@ -250,70 +266,58 @@ def word(wordid):
         query = "UPDATE Words SET maori = ?, english =?, level= ?, definition = ?, timestamp = ?, user_id = ?, " \
                 "username = ? WHERE id = ? "
         cur = con.cursor()
-        cur.execute(query, (maori, english, level, definition, timestamp, user_id, username, word_id)) # execute the query
+        cur.execute(query,
+                    (maori, english, level, definition, timestamp, user_id, username, word_id))  # execute the query
         con.commit()
         con.close()
-
 
     con = create_connection(DATABASE)
     query = "SELECT id, maori, english, definition, level, timestamp, user_id, image, username FROM Words WHERE id = ?"
     cur = con.cursor()
     cur.execute(query, (wordid,))
     words = cur.fetchall()
-    con.close
+    con.close()
 
-    # connect to database
-    con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC"  # select category from database
-    cur = con.cursor()
-    cur.execute(query)  # execute the query
-    category_list = cur.fetchall()
-    cur.close()
+    return render_template('word.html', logged_in=is_logged_in(), categories=category_list(), words=words,
+                           logged_in_teacher=is_logged_in_teacher())
 
-    return render_template('word.html', logged_in=is_logged_in(), categories=category_list, words=words, logged_in_teacher=is_logged_in_teacher())
 
+# app route for deleting worlds
 @app.route('/deleteword/<word_id>/<maori>')
 def delete_word(word_id, maori):
-
-
+    # connect database
     con = create_connection(DATABASE)
-    query = "SELECT id, maori, english, definition, level, timestamp, user_id, image, username FROM Words WHERE id = ?"
+    query = "SELECT id, maori, english, definition, level, timestamp, user_id, image, username FROM Words WHERE id = ?"  # select data from words
     cur = con.cursor()
     cur.execute(query, (word_id,))
-    words = cur.fetchall()
-    con.close
+    words = cur.fetchall()  # pass the data to the word.html
+    con.close()
 
-    # connect to database
-    con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC"  # select category from database
-    cur = con.cursor()
-    cur.execute(query)  # execute the query
-    category_list = cur.fetchall()
-    cur.close()
+    return render_template("deleteword.html", categories=category_list(), logged_in=is_logged_in(), words=words,
+                           logged_in_teacher=is_logged_in_teacher())
 
-    return render_template("deleteword.html", categories=category_list, logged_in=is_logged_in(), words=words, logged_in_teacher=is_logged_in_teacher())
 
+# app route for the delete button
 @app.route('/deleteword/<word_id>')
 def deleteword(word_id):
     print("Remove: {}".format(word_id))
-    query = "DELETE FROM Words WHERE id =? ;"
-    con = create_connection(DATABASE)
+    query = "DELETE FROM Words WHERE id =? ;"  # delete the data in the database
+    con = create_connection(DATABASE)  # creat connection to the database
     cur = con.cursor()
-    cur.execute(query, (word_id,))
+    cur.execute(query, (word_id,))  # execute the query
     con.commit()
     con.close()
     return redirect('/')
 
+
+# app route to delete category
 @app.route('/deletecategory/<category_id>/<category>')
 def delete_category(category_id, category):
-    con = create_connection(DATABASE)
-    query = "SELECT id, category FROM categories ORDER BY category ASC"
+    con = create_connection(DATABASE)  # connect to the database
     cur = con.cursor()
-    cur.execute(query)
-    category_list = cur.fetchall()
-    query = "SELECT id, category FROM categories WHERE id = ?"
-    cur.execute(query, (category_id,))
-    Categories = cur.fetchall()
+    query = "SELECT id, category FROM categories WHERE id = ?"  # select data from categories
+    cur.execute(query, (category_id,))  # execute the query
+    Categories = cur.fetchall()  # put result into a list
 
     # Select the data from my database
     query = "SELECT id, maori, english, image FROM Words WHERE category_id=? ORDER BY maori ASC"
@@ -321,41 +325,39 @@ def delete_category(category_id, category):
     cur = con.cursor()
     cur.execute(query, (category_id,))  # executes the query
     words_list = cur.fetchall()  # put result into a list
-    con.close
+    con.close()
 
-    return render_template("deletecategory.html", categories=category_list, logged_in=is_logged_in(), Categories=Categories, words_list=words_list, logged_in_teacher=is_logged_in_teacher())
+    return render_template("deletecategory.html", categories=category_list(), logged_in=is_logged_in(),
+                           Categories=Categories, words_list=words_list, logged_in_teacher=is_logged_in_teacher())
 
+
+# app route for the delete category button
 @app.route('/deletecategory/<category_id>')
 def deletecategory(category_id):
     print("Remove: {}".format(category_id))
-    query = "DELETE FROM Words WHERE category_id =? ;"
-    con = create_connection(DATABASE)
+    query = "DELETE FROM Words WHERE category_id =? ;"  # delete all the words
+    con = create_connection(DATABASE)  # connect to the database
     cur = con.cursor()
-    cur.execute(query, (category_id,))
+    cur.execute(query, (category_id,))  # execute the query
     con.commit()
-    query = "DELETE FROM categories WHERE id =? ;"
-    con = create_connection(DATABASE)
+    query = "DELETE FROM categories WHERE id =? ;"  # delete the categories
+    con = create_connection(DATABASE)  # connect to the database
     cur = con.cursor()
-    cur.execute(query, (category_id,))
+    cur.execute(query, (category_id,))  # execute the query
     con.commit()
     con.close()
     return redirect('/')
 
-def is_logged_in():
-    if session.get("email") is None:
-        print("not logged in")
-        return False
-    else:
-        print("logged in")
-        return True
 
-def is_logged_in_teacher():
-    if session.get("teacher") == "Y":
-        print("logged in as teacher")
-        return True
-    else:
-        print("logged in as student")
-        return False
+@app.route('/search')
+def search():
+    if request.method == 'POST':
+        print(request.form)
+        search_word = request.form.get('search').title().strip()  # gets the search from
 
+    return render_template('search.html', categories=category_list(), logged_in=is_logged_in(),
+                           logged_in_teacher=is_logged_in_teacher(), )
+
+# run the website
 if __name__ == '__main__':
     app.run()
